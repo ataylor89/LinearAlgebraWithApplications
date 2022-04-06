d768f49ddad903b2275692a46cbc44738c14bf17
Andrew Taylor <rolyata47@gmail.com>
2022-04-06 13:45:07 -0500

    Updating figures

--- ch2sec2figures.py
+++ ch2sec2figures.py
@@ -1,7 +1,7 @@
 from transformL import transformL
 from transformL import plotL
 plotL(1, 2, 'The letter L', 'L.png')
-transformL(1, 2, [[0.5, 0], [0, 0.5]], 'Scale by 0.5', 'Lscalebyhalf.png')
-transformL(1, 2, [[0, -1], [1, 0]], 'Rotate ninety degrees counterclockwise', 'Lrot90.png')
-transformL(1, 2, [[-1, 0], [0, 1]], 'Reflect about the Y axis', 'Lreflecty.png')
-transformL(1, 2, [[1, 0], [0, -1]], 'Reflect about the X axis', 'Lreflectx.png')
+transformL(1, 2, [[0.5, 0], [0, 0.5]], 'Scale L by 0.5', 'Lscalebyhalf.png')
+transformL(1, 2, [[0, -1], [1, 0]], 'Rotate L ninety degrees counterclockwise', 'Lrot90.png')
+transformL(1, 2, [[-1, 0], [0, 1]], 'Reflect L about the Y axis', 'Lreflecty.png')
+transformL(1, 2, [[1, 0], [0, -1]], 'Reflect L about the X axis', 'Lreflectx.png')

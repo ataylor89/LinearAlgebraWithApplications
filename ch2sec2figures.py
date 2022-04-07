e0fdb20299d0866999b5346fe8071fb7d8f0097e
Andrew Taylor <rolyata47@gmail.com>
2022-04-07 07:31:28 -0500

    Adding two problems, finding the orthogonal projection of L onto the X and Y axes

--- ch2sec2figures.py
+++ ch2sec2figures.py
@@ -7,3 +7,5 @@ transformL(1, 2, [[0, -1], [1, 0]], 'Rotate L ninety degrees counterclockwise', 
 transformL(1, 2, [[-1, 0], [0, 1]], 'Reflect L about the Y axis', 'Lreflecty.png')
 transformL(1, 2, [[1, 0], [0, -1]], 'Reflect L about the X axis', 'Lreflectx.png')
 transformL(1, 2, [[math.cos(math.pi/4), -1 * math.sin(math.pi/4)], [math.sin(math.pi/4), math.cos(math.pi/4)]], 'Rotate L forty five degrees counterclockwise', 'Lrot45.png')
+transformL(1, 2, [[1, 0], [0, 0]], 'Orthogonal projection of L onto the X axis', 'Lorthogonalprojx.png')
+transformL(1, 2, [[0, 0], [0, 1]], 'Orthogonal projection of L onto the Y axis', 'Lorthogonalprojy.png')

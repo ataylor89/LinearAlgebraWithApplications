01154667082200cfb6eb1cc078f0730cd348c7ee
Andrew Taylor <rolyata47@gmail.com>
2022-04-07 06:35:09 -0500

    Adding a problem and a solution

--- ch2sec2figures.py
+++ ch2sec2figures.py
@@ -1,7 +1,9 @@
 from transformL import transformL
 from transformL import plotL
+import math
 plotL(1, 2, 'The letter L', 'L.png')
 transformL(1, 2, [[0.5, 0], [0, 0.5]], 'Scale L by 0.5', 'Lscalebyhalf.png')
 transformL(1, 2, [[0, -1], [1, 0]], 'Rotate L ninety degrees counterclockwise', 'Lrot90.png')
 transformL(1, 2, [[-1, 0], [0, 1]], 'Reflect L about the Y axis', 'Lreflecty.png')
 transformL(1, 2, [[1, 0], [0, -1]], 'Reflect L about the X axis', 'Lreflectx.png')
+transformL(1, 2, [[math.cos(math.pi/4), -1 * math.sin(math.pi/4)], [math.sin(math.pi/4), math.cos(math.pi/4)]], 'Rotate L forty five degrees counterclockwise', 'Lrot45.png')
